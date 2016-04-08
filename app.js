9f30cf524ad5a135245d262fd2fbd51a897a6f01
Genevieve <gkolve@gmail.com>
2016-04-08 15:33:47 -0700

    [feature] adds slick carousel

---
+++ app.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+
+  $('.single-item').slick({
+  	  dots: true,
+  		infinite: true,
+  		speed: 300,
+  		slidesToShow: 1,
+  		slidesToScroll: 1,
+  		centerMode: true,
+  		arrows: true,
+  		accessability: true
+  });
+
+
+
+});
+
