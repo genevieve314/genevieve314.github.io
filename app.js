03c7b52a3f0bc6c51c709387482627266944fa27
Genevieve <gkolve@gmail.com>
2016-04-17 20:57:47 -0700

    [feature] adds on click to menue

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 
 $(document).ready(function(){
-
+	$('#Game').click(function(){
+		console.log('in on click')
+		var el = document.getElementById('GameSwap')
+		el.scrollIntoView(false);
+	})
 
 });
