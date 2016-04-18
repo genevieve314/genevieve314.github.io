0f26dbdc1df326d01d1e45a8f5851f6f88b0e463
Genevieve <gkolve@gmail.com>
2016-04-17 19:31:56 -0700

    [refactor] updates carousel styling

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 
 $(document).ready(function(){
 
-	$('#Glide').glide({
-		type: 'carousel',
-		autoplay: false,
-  	arrowsWrapperClass: 'slider-arrows',
-  	arrowRightText: '',
-  	arrowLeftText: '',
-  	centered: true
-	});
 
 });
