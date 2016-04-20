dd6129470b3e804e1ecca240e3b97f29ff49cf7b
Genevieve <gkolve@gmail.com>
2016-04-20 14:30:59 -0700

    [chore] creates test server

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 
 $(document).ready(function(){
+	$('#GS').load('assets/views/gameswap.html');
+
 	$('#Game').click(function(){
 		console.log('in on click')
 		var el = document.getElementById('GameSwap')
 		el.scrollIntoView(false);
-	})
+	});
+
+	$('#Liven').click(function(){
+		var el = document.getElementById('LivenUp')
+		el.scrollIntoView(false);
+	});
 
 });
